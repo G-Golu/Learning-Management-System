991181fdc85dcef1be211c2361d01a0f75d4802d
Golu Kumar <golug73700@gmail.com>
2025-01-08 22:25:05 +0530

    Add file pages , student , course detail, courses, home, payment-return, student-courses and install some packages

---
+++ Fronted/src/pages/instructor/index.jsx
@@ -0,0 +1,94 @@
+import InstructorCourses from "@/components/instructor-view/courses";
+import InstructorDashboard from "@/components/instructor-view/dashboard";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import { AuthContext } from "@/context/auth-context";
+import { InstructorContext } from "@/context/instructor-context";
+import { fetchInstructorCourseListService } from "@/services";
+import { BarChart, Book, LogOut } from "lucide-react";
+import { useContext, useEffect, useState } from "react";
+
+function InstructorDashboardpage() {
+  const [activeTab, setActiveTab] = useState("dashboard");
+  const { resetCredentials } = useContext(AuthContext);
+  const { instructorCoursesList, setInstructorCoursesList } =
+    useContext(InstructorContext);
+
+  async function fetchAllCourses() {
+    const response = await fetchInstructorCourseListService();
+    if (response?.success) setInstructorCoursesList(response?.data);
+  }
+
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  const menuItems = [
+    {
+      icon: BarChart,
+      label: "Dashboard",
+      value: "dashboard",
+      component: <InstructorDashboard listOfCourses={instructorCoursesList} />,
+    },
+    {
+      icon: Book,
+      label: "Courses",
+      value: "courses",
+      component: <InstructorCourses listOfCourses={instructorCoursesList} />,
+    },
+    {
+      icon: LogOut,
+      label: "Logout",
+      value: "logout",
+      component: null,
+    },
+  ];
+
+  function handleLogout() {
+    resetCredentials();
+    sessionStorage.clear();
+  }
+
+  console.log(instructorCoursesList, "instructorCoursesList");
+
+  return (
+    <div className="flex h-full min-h-screen bg-gray-100">
+      <aside className="hidden w-64 bg-white shadow-md md:block">
+        <div className="p-4">
+          <h2 className="mb-4 text-2xl font-bold">Instructor View</h2>
+          <nav>
+            {menuItems.map((menuItem) => (
+              <Button
+                className="justify-start w-full mb-2"
+                key={menuItem.value}
+                variant={activeTab === menuItem.value ? "secondary" : "ghost"}
+                onClick={
+                  menuItem.value === "logout"
+                    ? handleLogout
+                    : () => setActiveTab(menuItem.value)
+                }
+              >
+                <menuItem.icon className="w-4 h-4 mr-2" />
+                {menuItem.label}
+              </Button>
+            ))}
+          </nav>
+        </div>
+      </aside>
+      <main className="flex-1 p-8 overflow-y-auto">
+        <div className="mx-auto max-w-7xl">
+          <h1 className="mb-8 text-3xl font-bold">Dashboard</h1>
+          <Tabs value={activeTab} onValueChange={setActiveTab}>
+            {menuItems.map((menuItem) => (
+              <TabsContent value={menuItem.value}>
+                {menuItem.component !== null ? menuItem.component : null}
+              </TabsContent>
+            ))}
+          </Tabs>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default InstructorDashboardpage;
